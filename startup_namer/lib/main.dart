7bb2462d4e50fcb6eab9f1ec150c2bf0bb5efa61
BOGUENSONG <idsonic95@gmail.com>
2020-12-30 17:23:25 +0900

    add favorite icon

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -19,13 +19,28 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 22.0, color: Colors.blue[300]);
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
